022c2b2caceccc9e7e8ab0729a7e172b95d916f7
axelbalda1709 <axelbalda1709@gmail.com>
2022-06-01 17:50:56 +0200

    implem folder added

---
+++ implem/pnr/INNOVUS_CODER/SCRIPTS/placement.tcl
@@ -0,0 +1,20 @@
+#////////////////////////////////////////////////////
+#SCRIPT DE PLACEMENT DES CELLULES
+#Modified : 20 May 2022
+#////////////////////////////////////////////////////
+
+#Utilisation d'un noeud technologique supérieur ou égal à 130 nm
+setDesignMode -process 130
+
+#Placement des capa de découplage entre les rails d'alimentation vdd! gnd!
+addEndCap -preCap ENDCAPL -postCap ENDCAPR -prefix ENDCAP
+
+#Placement automatique des standard cells
+setPlaceMode -fp false
+placeDesign
+
+#report_constraint -all_violator
+
+
+
+
