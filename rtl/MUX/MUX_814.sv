0207268e35924d35243ece522cb495ca588a3484
Tom Desesquelle <tom.desesquelle@gmail.com>
2022-06-15 21:10:07 +0000

    Update rtl/MUX/MUX_814.sv

--- rtl/MUX/MUX_814.sv
+++ rtl/MUX/MUX_814.sv
@@ -16,8 +16,8 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-module MUX414 (    
-    input	[15 : 0]	inData  ,
+module MUX814 (    
+    input	[31 : 0]	inData  ,
     input	[1 : 0]	    inSel   ,      
     output	[3 : 0]		outData  
 ) ;
@@ -26,13 +26,17 @@ reg s_data0;
 reg s_data1;
 reg s_data2;
 reg s_data3;
+reg s_data4;
+reg s_data5;
+reg s_data6;
+reg s_data7;
 
 
 /////////////////////////////////////////////////////
 //  MUX 0
 /////////////////////////////////////////////////////
 MUX411 mux0(
-            .inData  ( {inData[12], inData[8], inData[4], inData[0]}   ) ,
+            .inData  ( {inData[24], inData[16], inData[8], inData[0]}   ) ,
             .inSel   ( inSel                                            ) ,
             .outData ( s_data0                                          ) 
             ) ;
@@ -42,7 +46,7 @@ MUX411 mux0(
 //  MUX 1
 /////////////////////////////////////////////////////
 MUX411 mux1(
-            .inData  ( {inData[13], inData[9], inData[5], inData[1]}   ) ,
+            .inData  ( {inData[25], inData[17], inData[9], inData[1]}   ) ,
             .inSel   ( inSel                                            ) ,
             .outData ( s_data1                                          ) 
             ) ;
@@ -52,7 +56,7 @@ MUX411 mux1(
 //  MUX 2
 /////////////////////////////////////////////////////
 MUX411 mux2(
-            .inData  ( {inData[14], inData[10], inData[6], inData[2]}    ) ,
+            .inData  ( {inData[26], inData[18], inData[10], inData[2]}    ) ,
             .inSel   ( inSel                                            ) ,
             .outData ( s_data2                                          ) 
             ) ;
@@ -62,12 +66,51 @@ MUX411 mux2(
 //  MUX 3
 /////////////////////////////////////////////////////
 MUX411 mux3(
-            .inData  ( {inData[15], inData[11], inData[7], inData[3]}    ) ,
+            .inData  ( {inData[27], inData[19], inData[11], inData[3]}    ) ,
             .inSel   ( inSel                                            ) ,
             .outData ( s_data3                                          ) 
             ) ;
 /////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////
+//  MUX 3
+/////////////////////////////////////////////////////
+MUX411 mux3(
+            .inData  ( {inData[28], inData[20], inData[12], inData[4]}    ) ,
+            .inSel   ( inSel                                            ) ,
+            .outData ( s_data3                                          ) 
+            ) ;
+/////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+//  MUX 3
+/////////////////////////////////////////////////////
+MUX411 mux3(
+            .inData  ( {inData[29], inData[21], inData[13], inData[5]}    ) ,
+            .inSel   ( inSel                                            ) ,
+            .outData ( s_data3                                          ) 
+            ) ;
+/////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+//  MUX 3
+/////////////////////////////////////////////////////
+MUX411 mux3(
+            .inData  ( {inData[30], inData[22], inData[14], inData[6]}    ) ,
+            .inSel   ( inSel                                            ) ,
+            .outData ( s_data3                                          ) 
+            ) ;
+/////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+//  MUX 3
+/////////////////////////////////////////////////////
+MUX411 mux3(
+            .inData  ( {inData[31], inData[23], inData[15], inData[7]}    ) ,
+            .inSel   ( inSel                                            ) ,
+            .outData ( s_data3                                          ) 
+            ) ;
+/////////////////////////////////////////////////////
 assign outData = {s_data3, s_data2, s_data1, s_data0} ;
 
 endmodule
