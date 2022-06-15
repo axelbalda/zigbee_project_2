47947e857d5d23d1b7b416dda951f0cb6269fc01
gitporrete <elisabeth-porret@hotmail.fr>
2022-06-15 13:34:53 +0200

    Back-up 15/06 - Modification of sensitivity list in always loops (posedge i_clk)

--- rtl/CDR/counter.sv
+++ rtl/CDR/counter.sv
@@ -7,7 +7,8 @@ module counter(
 	output reg 		o_en_m,		//2nd sampling
 	output reg 		o_en_f,		//3rd sampling
 	output reg 		o_en,       	//en : to enable sampling of T and E signals in phase detector
-	output reg 		o_en_freq_synch	//to enable modification of i_nb_P in divN
+	output reg 		o_en_freq_synch,	//to enable modification of i_nb_P in divN
+	output reg		o_en_dec
 	);
 
 reg [5:0] cnt;
@@ -20,6 +21,7 @@ always_ff@(posedge i_clk) begin
 		o_en_f <= 1'b0;
 		o_en <= 1'b0;
 		o_en_freq_synch <= 1'b0;
+		o_en_dec <= 1'b0;
 		cnt <= 1'b0;
 	end
 	else begin
@@ -28,6 +30,7 @@ always_ff@(posedge i_clk) begin
 		o_en_d <= 1'b0;
 		o_en_f <=1'b0;
 		o_en_freq_synch <= 1'b0;
+		o_en_dec <= 1'b0;
 
 		if (cnt == 1'b1) begin //1
 			o_en_d <= 1'b1; //debut
@@ -38,6 +41,10 @@ always_ff@(posedge i_clk) begin
 			o_en_m <= 1'b1; //milieu
 		end
 
+		if (cnt == (i_nb_P-1)/2) begin
+			o_en_dec <= 1'b1;
+		end
+
 		if (cnt == i_nb_P-4) begin //21
 			o_en_f <= 1'b1; //fin
 		end
@@ -46,17 +53,16 @@ always_ff@(posedge i_clk) begin
 			o_en <= 1'b1; // to enable sampling of T and E signals (=outputs of phase detector)
 		end
 
-		if (cnt == i_nb_P-1) begin //23
+		if (cnt == i_nb_P-2) begin //23
 			o_en_freq_synch <= 1'b1;
 		end
 		
 		if (cnt == i_nb_P-1) begin //24
 			cnt <= 1'b0;
 		end
-		else begin
-			if(i_cnt_d == 3) begin
-				cnt <= cnt + 1;
-			end
+
+		if((i_cnt_d == 3) && (cnt != i_nb_P-1)) begin
+			cnt <= cnt + 1;
 		end
 	end
 end
